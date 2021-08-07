93f48d757d4b7471ff79cfd3df9fb4c46d68ae42
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-07 18:42:01 -0400

    Cleaned up json file, made mod entrypoint final

--- src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
+++ src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
@@ -3,7 +3,7 @@ package me.roundaround.pickupnotifications;
 import me.roundaround.pickupnotifications.client.gui.hud.PickupNotificationsHud;
 import net.fabricmc.api.ClientModInitializer;
 
-public class PickupNotificationsMod implements ClientModInitializer {
+public final class PickupNotificationsMod implements ClientModInitializer {
     private final PickupNotificationsHud pickupNotificationsHud = new PickupNotificationsHud();
 
     @Override
