59641f876488971d76b3f03cf791b4ddfb5e3150
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-20 14:59:32 -0400

    Render list widget on ConfigScreen, render default Screen background

--- src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
+++ src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
@@ -4,16 +4,12 @@ import me.roundaround.pickupnotifications.client.gui.hud.PickupNotificationsHud;
 import me.roundaround.pickupnotifications.config.PickupNotificationsConfig;
 import me.roundaround.roundalib.util.ModInfo;
 import net.fabricmc.api.ClientModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public final class PickupNotificationsMod implements ClientModInitializer {
     public static final String MOD_ID = "me.roundaround.pickupnotifications";
     public static final String VERSION = "1.0.0";
 
-    public static final ModInfo MOD_INFO = new ModInfo(MOD_ID, VERSION, 1, "me.roundaround.pickupnotifications.modname", "me.roundaround.pickupnotifications.config.title");
-
-    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static final ModInfo MOD_INFO = new ModInfo(MOD_ID, VERSION, 1, "modname", "config.title");
     public static final PickupNotificationsConfig CONFIG = new PickupNotificationsConfig();
 
     private final PickupNotificationsHud pickupNotificationsHud = new PickupNotificationsHud();
--- src/main/java/me/roundaround/pickupnotifications/config/PickupNotificationsConfig.java
+++ src/main/java/me/roundaround/pickupnotifications/config/PickupNotificationsConfig.java
@@ -1,6 +1,6 @@
 package me.roundaround.pickupnotifications.config;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 import me.roundaround.pickupnotifications.PickupNotificationsMod;
 import me.roundaround.roundalib.config.ModConfig;
 import me.roundaround.roundalib.config.option.ConfigOption;
@@ -8,20 +8,20 @@ import me.roundaround.roundalib.config.option.IntConfigOption;
 import me.roundaround.roundalib.config.option.OptionListConfigOption;
 import me.roundaround.roundalib.config.value.GuiAlignment;
 
-import java.util.Collection;
-
 public class PickupNotificationsConfig extends ModConfig {
-    public static final OptionListConfigOption<GuiAlignment> GUI_ALIGNMENT = new OptionListConfigOption<>("guiAlignment", GuiAlignment.TOP_LEFT);
-    public static final IntConfigOption GUI_OFFSET_X = new IntConfigOption("guiOffsetX", 4);
-    public static final IntConfigOption GUI_OFFSET_Y = new IntConfigOption("guiOffsetY", 4);
+    public static final OptionListConfigOption<GuiAlignment> GUI_ALIGNMENT = new OptionListConfigOption<>("guiAlignment", "config.label.gui_alignment", GuiAlignment.TOP_LEFT);
+    public static final IntConfigOption GUI_OFFSET_X = new IntConfigOption("guiOffsetX", "config.label.gui_offset_x", 4);
+    public static final IntConfigOption GUI_OFFSET_Y = new IntConfigOption("guiOffsetY", "config.label.gui_offset_y", 4);
+
+    private static final ImmutableList<ConfigOption<?>> ALL_CONFIG_OPTIONS = ImmutableList.of(GUI_ALIGNMENT, GUI_OFFSET_X, GUI_OFFSET_Y);
 
     public PickupNotificationsConfig() {
         super(PickupNotificationsMod.MOD_INFO);
     }
 
     @Override
-    protected Collection<ConfigOption<?>> getConfigOptions() {
-        return Lists.newArrayList(GUI_ALIGNMENT, GUI_OFFSET_X, GUI_OFFSET_Y);
+    public ImmutableList<ConfigOption<?>> getConfigOptions() {
+        return ALL_CONFIG_OPTIONS;
     }
 
     public void init() {
