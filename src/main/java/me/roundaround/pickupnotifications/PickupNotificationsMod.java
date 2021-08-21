dc69792b0ba1096eba5e300b736a37ac39d4cdd1
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-21 13:54:02 -0400

    Update to target new RoundaLib version, changed artifact version to specify MC version

--- src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
+++ src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
@@ -6,7 +6,7 @@ import me.roundaround.roundalib.util.ModInfo;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class PickupNotificationsMod implements ClientModInitializer {
-    public static final ModInfo MOD_INFO = new ModInfo("pickupnotifications", "0.0.1", 1, "modname", "config.title");
+    public static final ModInfo MOD_INFO = new ModInfo("pickupnotifications", "0.0.1", 1);
     public static final PickupNotificationsConfig CONFIG = new PickupNotificationsConfig();
 
     private final PickupNotificationsHud pickupNotificationsHud = new PickupNotificationsHud();
