a76ff9b646a2982f6c0310e28fc15669884a08cb
Evan Steinkerchner <esteinkerchner@gmail.com>
2021-08-21 13:26:47 -0400

    Some gradle, modid, and version cleanup

--- src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
+++ src/main/java/me/roundaround/pickupnotifications/PickupNotificationsMod.java
@@ -6,10 +6,7 @@ import me.roundaround.roundalib.util.ModInfo;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class PickupNotificationsMod implements ClientModInitializer {
-    public static final String MOD_ID = "me.roundaround.pickupnotifications";
-    public static final String VERSION = "1.0.0";
-
-    public static final ModInfo MOD_INFO = new ModInfo(MOD_ID, VERSION, 1, "modname", "config.title");
+    public static final ModInfo MOD_INFO = new ModInfo("pickupnotifications", "0.0.1", 1, "modname", "config.title");
     public static final PickupNotificationsConfig CONFIG = new PickupNotificationsConfig();
 
     private final PickupNotificationsHud pickupNotificationsHud = new PickupNotificationsHud();
