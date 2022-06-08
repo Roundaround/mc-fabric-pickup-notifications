7f3e2c203abf476f0ce01d5890ac83536abd5178
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-06-08 18:57:14 -0400

    Nearly fixed it

--- src/main/java/me/roundaround/pickupnotifications/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/roundaround/pickupnotifications/mixin/ClientPlayNetworkHandlerMixin.java
@@ -11,23 +11,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import me.roundaround.pickupnotifications.PickupNotificationsMod;
 import me.roundaround.pickupnotifications.event.ItemPickupCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.s2c.play.ItemPickupAnimationS2CPacket;
+import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ClientPlayNetworkHandlerMixin {
-  Set<Integer> handledItemEntities = new HashSet<>();
+  @Shadow
+  private MinecraftClient client;
 
   @Shadow
   private ClientWorld world;
 
   @Inject(method = "onItemPickupAnimation", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkThreadUtils;forceMainThread", shift = At.Shift.AFTER))
-  private void onItemPickupAnimationHead(ItemPickupAnimationS2CPacket packet, CallbackInfo info) {
+  private void onItemPickupAnimation(ItemPickupAnimationS2CPacket packet, CallbackInfo info) {
     Entity entity = world.getEntityById(packet.getCollectorEntityId());
     if (!(entity instanceof ClientPlayerEntity)) {
       return;
@@ -40,6 +44,34 @@ public abstract class ClientPlayNetworkHandlerMixin {
 
     ItemStack itemStack = ((ItemEntity) itemEntity).getStack().copy();
     itemStack.setCount(packet.getStackAmount());
+
+    PickupNotificationsMod.LOGGER.info("===================");
+    PickupNotificationsMod.LOGGER.info("onItemPickupAnimation");
+    PickupNotificationsMod.LOGGER.info(itemStack);
+    PickupNotificationsMod.LOGGER.info("===================");
+
+    ItemPickupCallback.EVENT.invoker().interact(itemStack);
+  }
+
+  @Inject(method = "onScreenHandlerSlotUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;setBobbingAnimationTime"))
+  private void onScreenHandlerSlotUpdate(ScreenHandlerSlotUpdateS2CPacket packet, CallbackInfo info) {
+    ClientPlayerEntity playerEntity = client.player;
+    ItemStack currentlyHeld = playerEntity.playerScreenHandler.getSlot(packet.getSlot()).getStack();
+
+    if (currentlyHeld.isEmpty()) {
+      return;
+    }
+
+    int difference = packet.getItemStack().getCount() - currentlyHeld.getCount();
+
+    ItemStack itemStack = currentlyHeld.copy();
+    itemStack.setCount(difference);
+
+    PickupNotificationsMod.LOGGER.info("===================");
+    PickupNotificationsMod.LOGGER.info("onScreenHandlerSlotUpdate");
+    PickupNotificationsMod.LOGGER.info(itemStack);
+    PickupNotificationsMod.LOGGER.info("===================");
+
     ItemPickupCallback.EVENT.invoker().interact(itemStack);
   }
 }
