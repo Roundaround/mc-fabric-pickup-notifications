5237f5fed7e602ddaa10f7585357f55ef8b084ce
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-06-08 22:15:32 -0400

    Removed unused imports

--- src/main/java/me/roundaround/pickupnotifications/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/roundaround/pickupnotifications/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,8 +1,5 @@
 package me.roundaround.pickupnotifications.mixin;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,7 +14,6 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.s2c.play.ItemPickupAnimationS2CPacket;
 import net.minecraft.network.packet.s2c.play.ScreenHandlerSlotUpdateS2CPacket;
