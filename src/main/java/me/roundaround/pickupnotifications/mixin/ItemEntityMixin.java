06521a4eedd3e731b9fea088fb3576865c46e89e
Roundaround <roundaroundna@gmail.com>
2025-04-07 11:43:45 -0400

    Remove unused @Shadow reference

--- src/main/java/me/roundaround/pickupnotifications/mixin/ItemEntityMixin.java
+++ src/main/java/me/roundaround/pickupnotifications/mixin/ItemEntityMixin.java
@@ -7,16 +7,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
-  @Shadow
-  public abstract ItemStack getStack();
-
   @Inject(
       method = "onPlayerCollision", at = @At(
       value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;increaseStat(Lnet/minecraft/stat/Stat;I)V"
